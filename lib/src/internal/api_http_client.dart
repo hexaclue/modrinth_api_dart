1c08535f897c8a10b71362977d0b9f903b5cb5a8
Qy <HIHIQY1@gmail.com>
2023-03-01 17:06:35 +0100

    Add close() @ ApiHttpClient. Add A11n header on reqs

--- lib/src/internal/api_http_client.dart
+++ lib/src/internal/api_http_client.dart
@@ -3,12 +3,23 @@ import "package:http/http.dart" as http;
 class ApiHttpClient extends http.BaseClient {
   final String userAgent;
   final http.Client _inner;
+  final String? apiKey;
 
-  ApiHttpClient(this.userAgent, this._inner);
+  ApiHttpClient(this.userAgent, this._inner, [this.apiKey]);
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
     request.headers["user-agent"] = userAgent;
+
+    if (apiKey != null) {
+      request.headers["Authorization"] = apiKey!;
+    }
+
     return _inner.send(request);
+  }
+
+  @override
+  void close() {
+    _inner.close();
   }
 }
