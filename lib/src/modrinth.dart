b2e1b514a4ae089a0732c97d878b82a972aa4574
Qy <HIHIQY1@gmail.com>
2023-03-02 13:02:50 +0100

    Add project name to requests

--- lib/src/modrinth.dart
+++ lib/src/modrinth.dart
@@ -1,15 +1,26 @@
+import 'dart:convert';
+
 import "package:http/http.dart" as http;
 import "package:modrinth_api/src/internal/api_http_client.dart";
 import "package:modrinth_api/src/requests/projects.dart";
 import "package:modrinth_api/src/requests/versions.dart";
 
 class IModrinthApi {
-  IModrinthApi([this.apiKey]) {
-    client = ApiHttpClient("@hihiqy1/modrinth_api_dart/0.0.0+infdev", http.Client());
+  IModrinthApi({required this.project, this.apiKey}) {
+    try {
+      ascii.encode(project);
+    } catch (e) {
+      throw ArgumentError("Project name must be in ASCII format. Currently, it contains non-ASCII characters.");
+    }
+
+    client = ApiHttpClient("@hihiqy1/modrinth_api_dart/0.0.0+infdev|user=$project", http.Client());
   }
 
   /// The api key to use for requests
   final String? apiKey;
+
+  /// The project name to use for requests
+  final String project;
 
   /// The internal http client
   late final http.Client client;
@@ -23,6 +34,12 @@ class IModrinthApi {
   static const String baseUrl = "https://api.modrinth.com/v2";
 }
 
+/// The main class for the Modrinth API
+///
+/// [project] is the project name to use for requests to the API. PLEASE use a unique name for your project. Include a version number if possible.
 class ModrinthApi extends IModrinthApi with ProjectsRequests, VersionsRequests {
-  ModrinthApi([super.apiKey]);
+  ModrinthApi({
+    required super.project,
+    super.apiKey,
+  });
 }
