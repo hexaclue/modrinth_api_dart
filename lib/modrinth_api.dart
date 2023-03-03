890639b3e353bff58eb5f7ea8f86c52e2c001bee
Qy <HIHIQY1@gmail.com>
2023-03-03 12:48:39 +0100

    Fix exports

--- lib/modrinth_api.dart
+++ lib/modrinth_api.dart
@@ -10,16 +10,22 @@ export "src/core/project/project_enums.dart";
 export "src/core/project/project_license.dart";
 export "src/core/project/project.dart";
 
-export "src/core/search/facet.dart";
 export "src/core/search/facet_builder.dart";
+export "src/core/search/facet.dart";
 export "src/core/search/search_result.dart";
 
-export "src/core/team/team_member.dart";
-export "src/core/team/team_permissions.dart";
-export "src/core/team/team_permissions_constants.dart";
+export "src/core/tags/tag_category.dart";
+export "src/core/tags/tag_donation_platform.dart";
+export "src/core/tags/tag_game_version.dart";
+export "src/core/tags/tag_license.dart";
+export "src/core/tags/tag_loader.dart";
 
-export "src/core/user/badges.dart";
+export "src/core/team/team_member.dart";
+export "src/core/team/team_permissions_constants.dart";
+export "src/core/team/team_permissions.dart";
+
 export "src/core/user/badges_constants.dart";
+export "src/core/user/badges.dart";
 export "src/core/user/notification.dart";
 export "src/core/user/payout_history_item.dart";
 export "src/core/user/payout_history.dart";
@@ -31,7 +37,4 @@ export "src/core/version/version_file.dart";
 export "src/core/version/version_hashes.dart";
 export "src/core/version/version.dart";
 
-export "src/core/tags/tag_category.dart";
-export "src/core/tags/tag_game_version.dart";
-export "src/core/tags/tag_license.dart";
-export "src/core/tags/tag_loader.dart";
+export "src/core/instance_statistics.dart";
